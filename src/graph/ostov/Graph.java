3542ccd03915e6ce15116bdbd316d14bda591ada
roshik8 <roshik8@gmail.com>
2024-07-28 22:04:26 +0500

    добавление комментариев к алгоритму Крускала

--- src/graph/ostov/Graph.java
+++ src/graph/ostov/Graph.java
@@ -13,6 +13,7 @@ public class Graph {
     public Graph(int[][] matrix) {
         this.matrix = matrix;
         this.size = matrix.length;
+        //пустой подграф, который будет использоваться для хранения остовного дерева
         edges = new ArrayDeque<>();
     }
 
@@ -23,26 +24,36 @@ public class Graph {
 
     public void kruskal() {
         weightMin = 0;
+        //создаем список ребер нашего графа с весами
         List<Edge> edgeList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
+                //если пути из одной вершины в другую нету, то и ребра нет, выбрасываем
                 if (matrix[i][j] == 0) continue;
                 Edge edge = new Edge(i, j, matrix[i][j]);
                 edgeList.add(edge);
             }
         }
+        //сортируем все ребра по их весу, используя встроенную функцию сравнению числе в джаве. Сортируем просто чтобы по-порядку присоединять, от меньшего к большему, чтобы получить минимальный вес
         edgeList.sort(Comparator.comparingInt(o -> o.weight));
 
+        // создаем массив родителей для каждой вершины, для того чтобы определить системы непересекающихся множеств, чтобы не было циклов в нашем остовном дереве
         parents = new int[size];
+        //изначально у каждой вершины родитель это сама вершина
         for (int i = 0; i < size; i++) {
             parents[i] = i;
         }
+        //перебираем наши отсортированные ребра
         for (Graph.Edge edge : edgeList) {
+            //рекурсивно определяем родителя для начальной и конечной вершин
             int begin = getParent(edge.begin);
             int end = getParent(edge.end);
+            //проверка того что ребро относится к одной компоненте связности, если да то переходим к следующей итерации цикла для проверки новых ребер
             if (begin == end) continue;
+            //если к разным, значит присоединяем ребро к нашему минимальному остовному деревеу
             edges.add(edge);
             weightMin += edge.weight;
+            //ставим родителем у конечной вершины - начальную, чтобы дальше присоединять ребра
             parents[end] = begin;
         }
     }
